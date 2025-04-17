902c4273020af477fffad0e927e73a54a73b3b8f
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2025-04-17 17:15:16 +0900

    Update TrumpGunItem.java

--- src/main/kid_gun_1412/item/TrumpGunItem.java
+++ src/main/kid_gun_1412/item/TrumpGunItem.java
@@ -1,1 +1,9 @@
+package kid_gun_1412.item;
 
+import net.minecraft.world.item.Item;
+
+public class TrumpGunItem extends Item {
+    public TrumpGunItem(Properties properties) {
+        super(properties);
+    }
+}
