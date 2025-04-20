1c50e445917b4b579248f44dcececb5136ebecbe
0526ngs <hrmc.ngs@gmail.com>
2025-04-21 07:43:21 +0900

    サンプル
    名前変えないと

--- src/main/kid_gun_1412/item/TrumpGunItem.java
+++
@@ -1,9 +0,0 @@
-package kid_gun_1412.item;
-
-import net.minecraft.world.item.Item;
-
-public class TrumpGunItem extends Item {
-    public TrumpGunItem(Properties properties) {
-        super(properties);
-    }
-}
